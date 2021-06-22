cbb4e42ef0a9122f3aeb5a2d946b6908209c1b21
Alex Christenson <christenson10@gmail.com>
2021-06-21 23:37:42 -0400

    14.1.4

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./api/user-routes.js');
 const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
